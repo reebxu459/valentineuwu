5e4b43fa53225c1a34e2a64cd2162664f5f14339
reebxu459 <rebbi.xu123@gmail.com>
2024-02-14 00:40:06 -0500

    no more hard-coding

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
 import streamlit as st
-import numpy as np
-from PIL import Image
-
-# messages = ["",
-#             "come on, I'll give you a hug", 
-#             "fine, how about a kiss too?", 
-#             "I'll throw in a massage", 
-#             "how about a special message... :3", 
-#             "ok, I know what you really want. Dinner's on me",
-#             "think about the last one some more!",
-#             "FINE. Dinner AND dessert are on me"
-#             ]
 
 
 st.markdown("""
@@ -37,6 +25,45 @@ button_style = """
         """
 st.markdown(button_style, unsafe_allow_html=True)
 
+stages = {
+    0: {
+        'image': "assets/happy.png",
+        'message': "<p class='large-text'>Come on, I'll give you a hug</p>"
+    },
+    1: {
+        'image': "assets/stressed1.png",
+        'message': "<p class='large-text'>Come on, I'll give you a hug!</p>"
+    },
+    2: {
+        'image': "assets/stressed1.png",
+        'message': "<p class='large-text'>Fine, how about a kiss too?</p>"
+    },
+    3: {
+        'image': "assets/stressed2.png",
+        'message': "<p class='large-text'>I'll throw in a massage.</p>"
+    },
+    4: {
+        'image': "assets/resolution.png",
+        'message': "<p class='large-text'>How about a special massage :3</p>"
+    },
+    5: {
+        'image': "assets/stressed3.png",
+        'message': "<p class='large-text'>Okay, I know what you really want. Dinner's on me.</p>"
+    },
+    6: {
+        'image': "assets/stressed3.png",
+        'message': "<p class='large-text'>Come on, think about the last one some more!</p>"
+    },
+    7: {
+        'image': "assets/resolution.png",
+        'message': "<p class='large-text'>FINE. Dinner and dessert are both on me if you accept.</p>"
+    },
+    8: {
+        'image': "assets/muah.png",
+        'message': "<p class='title'>I LOVE LOVE LOVEEEE UUUUUU</p>"
+    }
+}
+
 if 'stage' not in st.session_state:
     st.session_state.stage = 0
 
@@ -46,78 +73,21 @@ def set_state(i):
 if st.session_state.stage < 8:
     st.markdown("<p class='title'>Wanna be my Valentine?</p>", unsafe_allow_html=True)
 
-if st.session_state.stage == 0:
-
-    image = "assets/happy.png"
-    st.image(image, use_column_width=True)
-    col1, col2 = st.columns(2)
-    with col1: st.button('Yes', on_click=set_state, args=[8])
-    with col2: 
-        if st.button('No', on_click=set_state, args=[1]): st.markdown("<p class='large-text'>Come on, I'll give you a hug</p>", unsafe_allow_html=True)
-
-if st.session_state.stage == 1:
-    image = "assets/stressed1.png"
+if st.session_state.stage < 8:
+    current_stage = stages[st.session_state.stage]
+    image = current_stage['image']
     st.image(image, use_column_width=True, width=0.8)
-    col1, col2 = st.columns(2)
-    with col1: st.button('Yes', on_click=set_state, args=[8])
-    with col2:
-        if st.button('No', on_click=set_state, args=[2]): st.markdown("<p class='large-text'>Come on, I'll give you a hug!</p>", unsafe_allow_html=True)
 
-if st.session_state.stage == 2:
-    image = "assets/stressed1.png"
-    st.image(image, use_column_width=True, width=0.8)
     col1, col2 = st.columns(2)
-    with col1: st.button('Yes', on_click=set_state, args=[8])
+    with col1: 
+        st.button('Yes', on_click=set_state, args=[8])
     with col2:
-        if st.button('No', on_click=set_state, args=[3]): st.markdown("<p class='large-text'>Fine, how about a kiss too?</p>", unsafe_allow_html=True)
-
-if st.session_state.stage == 3:
-    image = "assets/stressed2.png"
-    st.image(image, use_column_width=True, width=0.8)
-    col1, col2 = st.columns(2)
-    with col1: st.button('Yes', on_click=set_state, args=[8])
-    with col2:
-        if st.button('No', on_click=set_state, args=[4]): st.markdown("<p class='large-text'>I'll throw in a massage.</p>", unsafe_allow_html=True)
-
-if st.session_state.stage == 4:
-    image = "assets/resolution.png"
-    st.image(image, use_column_width=True, width=0.8)
-    col1, col2 = st.columns(2)
-    with col1: st.button('Yes', on_click=set_state, args=[8])
-    with col2:
-        if st.button('No', on_click=set_state, args=[5]): st.markdown("<p class='large-text'>How about a special massage :3</p>", unsafe_allow_html=True)
-
-if st.session_state.stage == 5:
-    image = "assets/stressed3.png"
-    st.image(image, use_column_width=True, width=0.8)
-    col1, col2 = st.columns(2)
-    with col1: st.button('Yes', on_click=set_state, args=[8])
-    with col2:
-        if st.button('No', on_click=set_state, args=[6]): st.markdown("<p class='large-text'>Okay, I know what you really want. Dinner's on me.</p>", unsafe_allow_html=True)
-
-if st.session_state.stage == 6:
-    image = "assets/stressed3.png"
-    st.image(image, use_column_width=True, width=0.8)
-    col1, col2 = st.columns(2)
-    with col1: st.button('Yes', on_click=set_state, args=[8])
-    with col2:
-        if st.button('No', on_click=set_state, args=[7]): st.markdown("<p class='large-text'>Come on, think about the last one some more!</p>", unsafe_allow_html=True)
-
-if st.session_state.stage == 7:
-    image = "assets/resolution.png"
-    st.image(image, use_column_width=True, width=0.8)
-    col1, col2 = st.columns(2)
-    with col1: st.button('Yes', on_click=set_state, args=[8])
-    with col2:
-        st.button('YES!!', on_click=set_state, args=[8])
-        st.markdown("<p class='large-text'>FINE. Dinner and dessert are both on me if you accept.</p>", unsafe_allow_html=True)
+        if st.button('No', on_click=set_state, args=[st.session_state.stage + 1]):
+            st.markdown(current_stage['message'], unsafe_allow_html=True)
 
 # HE SAID YES!!
 if st.session_state.stage >= 8:
     image = "assets/muah.png"
     st.image(image, use_column_width=True, width=0.8)
     st.balloons()
-    st.markdown("<p class='title'>I LOVE LOVE LOVEEEE UUUUUU</p>", unsafe_allow_html=True)
-    
-
-
+    st.markdown("<p class='title'>I LOVE LOVE LOVEEEE UUUUUU\nHAPPY VALENTINES DAY!!</p>", unsafe_allow_html=True)
